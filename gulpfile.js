6a817bfc3f7ddbc769e0d989ea0da55905bfdaf4
Ian Busko <ian.busko@gmail.com>
2017-07-12 21:52:07 -0400

    update gulpfile and package.json to include correct directories

--- gulpfile.js
+++ gulpfile.js
@@ -8,38 +8,30 @@ var browserSync = require('browser-sync');
 var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 
-var autopolyfiller = require('gulp-autopolyfiller');
-
 var LessPluginAutoPrefix = require('less-plugin-autoprefix');
 var autoprefixPlugin = new LessPluginAutoPrefix({browsers: ["last 2 versions", "iOS 7"]});
 
 gulp.task('default', ['watch']);
 gulp.task('build', function () {
-	return gulp.src('wwwroot/less/*.less')
+	return gulp.src('assets/less/*.less')
 		.pipe(less({
             plugins: [autoprefixPlugin],
 			paths: [ path.join(__dirname, 'less','includes')]
 		}))
-		.pipe(gulp.dest('wwwroot/css'))
+		.pipe(gulp.dest('assets/css'))
         .pipe(browserSync.stream());
 });
 
-gulp.task('autopolyfiller', function () {
-    return gulp.src('wwwroot/js/*.js')
-        .pipe(autopolyfiller('result_polyfill_file.js'))
-        .pipe(gulp.dest('./dist'));
-});
-
 gulp.task('watch', function(){
-	gulp.watch('wwwroot/less/*.less', ['build', 'minify-css']);
+	gulp.watch('assets/less/*.less', ['build', 'minify-css']);
 	return;
 });
 
 gulp.task('minify-css', ['build'], function () {
-    return gulp.src(['wwwroot/css/*.css', '!wwwroot/css/*.min.css'])
+    return gulp.src(['assets/css/*.css', '!assets/css/*.min.css'])
         .pipe(cleanCSS())
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('wwwroot/css'));
+        .pipe(gulp.dest('assets/css'));
 });
 
 gulp.task('serve', ['build', 'minify-css'], function () {
@@ -48,12 +40,12 @@ gulp.task('serve', ['build', 'minify-css'], function () {
         proxy: 'localhost:' + 5000,
         port: 5000,
         files: [
-            'wwwroot/less/*.less'
+            'assets/less/*.less'
         ],
         injectChanges: true,
         reloadDelay: 250,
     };
 
     browserSync(options);
-    gulp.watch('wwwroot/less/*.less', ['build', 'minify-css']);
+    gulp.watch('assets/less/*.less', ['build', 'minify-css']);
 });
